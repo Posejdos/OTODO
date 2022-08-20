052847b1d64ed384a5509e840f896cee41632b26
Marcin <marcin@sybet.pl>
2022-08-20 18:52:22 +0200

    Simple database

--- server/my_db.js
+++ server/my_db.js
@@ -13,6 +13,26 @@ const query = (command, method = 'all') => {
     }); 
 };
 
+const createTestUsers = async() => {
+	const existingUsers = await query('SELECT * from users');
+
+	if (existingUsers?.length === 0) {
+		let usernames = ['Jadwiga', 'Baltazar', 'Tirex'];
+		let passwords = ['Dupadupa', 'Manikir', 'jdiwoa'];
+		for (let i = 0; i < 3; i++) {
+			let name = usernames[i];
+			let pwd = passwords[i];
+			let id = i;
+
+			await query(`INSERT INTO users VALUES (${id}, "${name}", "${pwd}")`, 'run');
+		}
+	}
+};
+
 db.serialize(async () => {
     await query("CREATE TABLE IF NOT EXISTS users (id integer, username text, passwordHash text)", 'run');
+	await createTestUsers();
+
+	const existingUsers = await query('SELECT id, username, passwordHash FROM users');
+	console.log(existingUsers);
 });
