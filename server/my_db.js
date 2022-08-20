5c7873d49e1e86bd59d898af140d93beb76de048
Marcin <marcin@sybet.pl>
2022-08-20 19:41:02 +0200

    Add users, validate users

--- server/my_db.js
+++ server/my_db.js
@@ -1,4 +1,5 @@
 import sqlite3 from 'sqlite3'
+import bcrypt from 'bcrypt'
 
 const db = new sqlite3.Database('./data.db')
 const query = (command, method = 'all') => {
@@ -13,26 +14,42 @@ const query = (command, method = 'all') => {
     }); 
 };
 
+/* Needs to be sure that there's no user with that username */
+async function storeUser(username, password) {
+	const hash = await hashPassword(password)
+	await query(`INSERT INTO users VALUES ("${username}", "${hash}")`, 'run');
+}
+
+/* Needs to be sure that there's a user with that username */
+async function validateUser(username, password) {
+	const user = await query(`SELECT username, hash FROM users WHERE username = "${username}"`);
+	const currentHash = user[0].hash;
+
+	return (await bcrypt.compare(password, currentHash));
+}
+
+async function hashPassword(password) {
+	const hash = await bcrypt.hash(password, 10);
+	return hash;
+}
+
 const createTestUsers = async() => {
 	const existingUsers = await query('SELECT * from users');
 
 	if (existingUsers?.length === 0) {
-		let usernames = ['Jadwiga', 'Baltazar', 'Tirex'];
-		let passwords = ['Dupadupa', 'Manikir', 'jdiwoa'];
+		let usernames = ['TestUsr1', 'TestUsr2', 'TestUsr3'];
+		let passwords = ['TestPwd1', 'TestPwd2', 'TestPwd3'];
 		for (let i = 0; i < 3; i++) {
-			let name = usernames[i];
-			let pwd = passwords[i];
-			let id = i;
-
-			await query(`INSERT INTO users VALUES (${id}, "${name}", "${pwd}")`, 'run');
+			await storeUser(usernames[i], passwords[i]);
 		}
 	}
 };
 
 db.serialize(async () => {
-    await query("CREATE TABLE IF NOT EXISTS users (id integer, username text, passwordHash text)", 'run');
+    await query("CREATE TABLE IF NOT EXISTS users (username text, hash text)", 'run');
 	await createTestUsers();
 
-	const existingUsers = await query('SELECT id, username, passwordHash FROM users');
-	console.log(existingUsers);
+	const existingUsers = await query('SELECT rowid as id, username, hash FROM users');
+	console.log(await validateUser("TestUsr2", "TestPwd2"));
+	console.log(await validateUser("TestUsr2", "Testpwd2"));
 });
